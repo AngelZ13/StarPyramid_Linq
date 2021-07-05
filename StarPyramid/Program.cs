d7c59d7fe2a7cfa9f4e619bcdc10bb9ce92e8840
AngelZaf8 <73531566+AngelZaf8@users.noreply.github.com>
2021-07-05 07:22:24 +0300

    Add project files.

---
+++ StarPyramid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ChristmasTree(5)); 
+        }
+
+        public static string ChristmasTree(int height) => string.Join("\n", Enumerable.Range(1, height).Select(i => new string('*', 2 * i - 1).PadLeft(height + i - 1).PadRight(2 * height - 1)));
+        
+
+    }
+
+   
+
+
+}
